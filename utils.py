83a655e3c1f44518dc0e7e1ec6cd12fbc077d375
Diego Ubirajara <15200556+dubirajara@users.noreply.github.com>
2021-05-28 17:49:08 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -9,16 +9,16 @@ redis = redis.from_url(redis_url)
 
 
 def get_data():
-    if redis.get('date').decode("utf-8") != str(date.today()):
-        url = 'https://api.nasa.gov/planetary/apod'
-        payload = {'api_key': config('API_KEY')}
+    #if redis.get('date').decode("utf-8") != str(date.today()):
+     url = 'https://api.nasa.gov/planetary/apod'
+     payload = {'api_key': config('API_KEY')}
 
-        data = requests.get(url, params=payload).json()
+     data = requests.get(url, params=payload).json()
 
-        redis.set('date', data['date'])
-        redis.set('title', data['title'])
-        redis.set('image', data['hdurl'])
-        redis.set('details', data['explanation'])
+     redis.set('date', data['date'])
+     redis.set('title', data['title'])
+     redis.set('image', data['hdurl'])
+     redis.set('details', data['explanation'])
 
     data_content = [redis.get(key).decode("utf-8") for key in redis.keys()]
 
